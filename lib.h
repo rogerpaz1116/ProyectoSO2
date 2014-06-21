385f5741ca2a2940ed10edfdbef0190a5d0e91f7
rogerpaz1116 <rogerpaz_16@unitec.edu>
2014-06-20 19:55:44 -0600

    Pasé todas las funciones a lib

--- lib.h
+++ lib.h
@@ -1,3 +1,46 @@
+
+#define BLOCKSIZE 512
+#define MAGICNUMBER 1837083 
+#define DIRECTORYENTRYCOUNT 16
+#define ISDIRECTORY 1
+#define ISFILE 2
+#define ROOTBLOCK 35
+#define SUPERBLOCK 34
+#define FsStart 15
+typedef struct{
+	int magicNumber;
+	unsigned int totalBlocksInDisk;
+	unsigned int totalFreeBlocks;
+	unsigned int firstFreeBlock;
+	unsigned int lastFreeBlock;
+	char wastedSpace[492];
+}SuperBlock;
+
+
+typedef struct
+{
+	unsigned int nextFreeBlock;
+	char wastedSpace[492];
+}UnusedBlock;
+
+typedef struct
+{
+	char  fileName[20];
+	char fileType;
+	//unsigned int fileBlock;
+	unsigned int fileBlockStart;
+	unsigned int fileBlockSize;
+
+}DirectoryEntry;
+
+typedef struct 
+{
+	DirectoryEntry entries[16];
+}DirectoryBlock;
+
+
+
+
 void readsector(int, char*);
 void writesector(int, char*);
 void putchar(char);
@@ -21,5 +64,52 @@ void setchar(char,char,int,int);
 void setstring(char*,char,int,int);
 void getnumberstring(char*,int);
 
+
+
+//Funciones utilerías
+char *strcpy(char* , char*);
+
+int strCmp (const char *, const char *);
+
+int stlen(char * cadena, int start, char delim);
+
+int qtytoken(char * token, char delim);
+
+char * strtok(char * str, char delim, int pos);
+
+int mystrlen(char *cadena);
+
+char * mistrcat(char * destino,char * fuente);
+
+
+//Funciones de Basic
+void read_sector(int, char*);
+void write_sector(int, char*);
+
+
+
 //funciones del FS
+
+
+
+DirectoryBlock createEmptyDirectory();
+DirectoryBlock loadRootDirectory();
+int findFileInDirectory(DirectoryBlock ,char * );
+unsigned int findFileBlockInPath(char * );
+unsigned int allocateBlock();
+
+char * getPathName(char * path, int type);
+void setFreeBlock(unsigned int blockNumber);
+
+unsigned int createDir(char* filePath,char* fileName);
+DirectoryBlock listDir(char * filePath);
+void mkdirFS(char *name);
+void renameDir(char *path, char * newName);
+void copyDir(char *sourcePath, char *destinationPath);
+void deleteDirCascade(int block);
+void copyDirCascade(int blockSource, int blockDestination, char * destinationPath);
+void move (char* sourcePath, char* destinyPath);
+
+
+
 void ps();
